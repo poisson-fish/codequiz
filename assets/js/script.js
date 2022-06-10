cf2decc301c9e59524753b8353d14617bc2bc6c6
twin <twin@twin.com>
2022-06-10 16:47:28 -0600

    timer and progress bar working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,36 @@
-
+const startButton = document.getElementById("startButton");
 const carouselContainer = document.getElementById('carouselContainer');
-
+const carouselData = new bootstrap.Carousel(carouselContainer.parentElement, {
+    interval: false,
+    keyboard: false,
+    pause: false,
+    touch: false,
+    wrap: false
+});
+const cardDelay = 750;
 const quizQuestions = data;
 
 const savedState = localStorage.getItem('appstate');
 const appState = savedState ? savedState : {
     currentQuestion: 0,
-    activeCard: null,
     answers: []
 };
 
+var countdownTimer = 60.0;
+var countdownInterval;
 
-function createNewCarouselCard(question) {
+
+function createNewCarouselCard(question, index) {
     const carItem = document.createElement('div');
     carItem.className = 'carousel-item';
+    carItem.setAttribute("data-qid", index);
 
     const newCard = document.createElement('div');
     newCard.className = 'card text-center';
-    newCard.innerHTML = `<div class="card-header">
+    newCard.innerHTML = `
+<div class="card-header" id="cardHeader">
     <i class="fa fa-question-circle" aria-hidden="true"></i>
-    Quiz!
+    ${countdownTimer} seconds remaining!
 </div>
 <div class="card-body" id="cardBody">
     <h5 class="card-title" id="questionTitle">${question.title}</h5>
@@ -29,64 +40,80 @@ function createNewCarouselCard(question) {
 </div>
 <div class="card-footer text-muted">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-
-            </div>
-            <div class="col" style="text-align: center; ">
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="25"
-                        aria-valuemin="0" aria-valuemax="100">
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                
-            </div>
-        </div>
+            <div class="progress" style="width: 100%">
+            <div id="progressBar" class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+   
     </div>
 </div>`;
     const answerList = newCard.querySelector('#answerList');
-    question.options.forEach((answer, index) => {
-        //List item
-        const questionLi = document.createElement('li');
-        questionLi.className = 'list-group-item';
-        questionLi.setAttribute("data-answerid", index);
+    question.options
+        .map((value, index) => ({ val: value, sort: Math.random(), idx: index })) //put each element in the array in an object, and give it a random sort key along with its original position
+        .sort((a, b) => a.sort - b.sort) //sort using the random key
+        .forEach((answer) => {
+            //List item
+            const questionLi = document.createElement('li');
+            questionLi.className = 'list-group-item';
+            questionLi.setAttribute("data-answerid", answer.idx);
 
-        //Answer Button
+            //Answer Button
+            const questionButton = document.createElement('a');
+            questionButton.href = '#';
+            questionButton.className = 'btn btn-primary';
+            questionButton.style = 'width: 100%; height: 100%;'
+            questionButton.textContent = decodeURIComponent(answer.val);
+            questionButton.onclick = (e) => {
+                const answerid = e.target.parentElement.getAttribute('data-answerid');
+                const footer = newCard.querySelector("#cardBody");
 
-        const questionButton = document.createElement('a');
-        questionButton.href = '#';
-        questionButton.className = 'btn btn-primary';
-        questionButton.style = 'width: 100%; height: 100%;'
-        questionButton.textContent = decodeURIComponent(answer);
-        questionButton.onclick = (e) => {
-            const answer = e.target.parentElement.getAttribute('data-answerid');
-            const footer = newCard.querySelector("#cardBody");
+                if (answerid == question.answer) {
+                    displayAlert(footer, "Correct!", 'alert-success');
+                    setTimeout(() => {
+                        appState.currentQuestion++;
+                        carouselData.next();
+                    }, cardDelay);
+                }
+                else {
+                    displayAlert(footer, "Incorrect!", 'alert-danger');
+                    countdownTimer -= 10;
+                }
 
-            if (answer == question.answer) {
-                displayAlert(footer, "Correct!", 'alert-success');
-            }
-            else {
-                displayAlert(footer, "Incorrect!", 'alert-danger');
-            }
-
-        };
-        carItem.appendChild(newCard);
-        questionLi.appendChild(questionButton);
-        answerList.appendChild(questionLi);
-    });
+            };
+            carItem.appendChild(newCard);
+            questionLi.appendChild(questionButton);
+            answerList.appendChild(questionLi);
+        });
     return carouselContainer.appendChild(carItem);
 }
 
-function updateUI() {
+function start() {
     const questionNum = appState.currentQuestion;
     if (questionNum >= quizQuestions.length) return;
     const question = quizQuestions[questionNum];
 
-    createNewCarouselCard(question);
+    //Clear carousel 
+    //carouselContainer.innerHTML = '';
+    quizQuestions.forEach((question, index) => {
+        createNewCarouselCard(question, index);
+    });
 
-
+    const timeResolution = 0.5;
+    countdownTimer = 60.0;
+    countdownInterval = setInterval(() => {
+        countdownTimer -= timeResolution;
+        document.querySelectorAll("#cardHeader").forEach((timerText) => {
+            timerText.innerHTML = `
+            <i class="fa fa-question-circle" aria-hidden="true"></i>
+            ${Math.floor(countdownTimer)} seconds remaining!
+            `;
+        });
+        const currentCompletion = (appState.currentQuestion / quizQuestions.length) * 100;
+        document.querySelectorAll("#progressBar").forEach((progressBar) => 
+        {
+            progressBar.style = `width: ${currentCompletion}%`;
+            progressBar.ariaValueNow = currentCompletion;
+        });
+    }, timeResolution * 1000);
 
 }
 
@@ -116,12 +143,15 @@ function displayAlert(cardBody, alertText, alertType) {
     cardBody.appendChild(newAlert);
 
     const alertBs = new bootstrap.Alert(newAlert);
-    setTimeout((alert) => alert.close(), 5000, alertBs);
+    setTimeout((alert) => alert.close(), cardDelay, alertBs);
 }
 function answerCurrentQuestion(selectedAnswer) {
 
 }
 
-updateUI();
 
+startButton.onclick = (e) => {
+    start();
+    carouselData.next();
+};
 
